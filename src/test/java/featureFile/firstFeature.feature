eef9de2a97610a161d8f96d47b99e54bdc3633e6
Rahul <silimkarr@gmail.com>
2024-09-16 07:48:43 +0530

    16th commit

--- src/test/java/featureFile/firstFeature.feature
+++ src/test/java/featureFile/firstFeature.feature
@@ -5,7 +5,18 @@ Scenario: Admin Page default login
 	When User login into application with username "admin" and password "1234"
 	Then Home Page is displayed
 	And Cards are displayed
-	
+
+#DataDrivenTesting -> used in cases like forms,etc having more details
+Scenario: User page data fill up
+	Given User is on practice landing page
+	When User signs up into application
+	|Rahul|
+	|Shilimkar|
+	|silimkarr@gamil.com|
+	|7045821324|
+	Then Home Page is displayed
+	And Cards are displayed 
+
 #Using Scenario Outline and Regex, where there are multiple using for  a same flow using Parameterization
 Scenario Outline: Client Page default login 
 	Given User is on Netbanking login page
--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,5 +1,7 @@
 package stepDefinitions;
 
+import java.util.List;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,6 +12,28 @@ public class Steps {
 	 public void user_is_on_netbanking_login_page() {
 	     System.out.println("User is on Netbanking login page");
 	 }
+	 
+	 @Given("User is on practice landing page")
+	 public void user_is_on_practice_landing_page() {
+	     System.out.println("On datadriver homepage");
+	 }
+	 
+	 @When("User signs up into application")
+	 public void user_signs_up_into_application(List<String> formValue) {
+	     System.out.println(formValue.get(0));
+	     System.out.println(formValue.get(1));
+	     System.out.println(formValue.get(2));
+	     System.out.println(formValue.get(3));
+	 }
+//	 @When("User signs up into application")
+//	 public void user_signs_up_into_application(io.cucumber.datatable.DataTable dataTable) {
+//	     // For automatic transformation, change DataTable to one of
+//	     // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+//	     // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+//	     // Double, Byte, Short, Long, BigInteger or BigDecimal.
+//	     // For other transformations you can register a DataTableType.
+//	     throw new io.cucumber.java.PendingException();
+//	 }
 //	 @When("User login into application")
 //	 public void user_login_into_application() {
 //		 System.out.println("User login into application");
